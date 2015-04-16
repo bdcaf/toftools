4816622177d9d2db407c9cabc6235b3fe987bd8f
Clemens Ager <clemens.ager@gmail.com>
2015-04-16 14:27:30 +0200

    peaksuche

--- dev_only/dev class experiments.R
+++ dev_only/dev class experiments.R
@@ -1,6 +1,7 @@
-source('dev_only/dev class.R')
-source('dev_only/TofGetScan.R')
 source('R/tofReader.R')
+source('R/TofMeasurementClass.R')
+source('R/TofGetScan.R')
+source('R/massCalFunctions.R')
 
 tof.h5 <- file.path('testdata',"Ac just exhale (2014-10-23T10h43m42_#).h5")
 
@@ -8,4 +9,17 @@ tof.h5 <- file.path('testdata',"Ac just exhale (2014-10-23T10h43m42_#).h5")
 tm <- createTofMeasurement(file=tof.h5)
 sc <- getScan(tm,100)
 
-plot(sc, type='l')
+plot(sc)
+
+# spectra <- transformIntensity(sc, method="sqrt")
+
+spectra <- smoothIntensity(sc, method="SavitzkyGolay", halfWindowSize=3)
+
+plot(spectra)
+
+
+# baseline <- estimateBaseline(spectra, method="SNIP", iterations=100)
+# 
+# plot(baseline)
+#
+# is all 0!
