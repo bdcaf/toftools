b70f6afd389c3b1508abce200b5323452a3a4a5f
Clemens Ager <clemens.ager@gmail.com>
2015-04-16 13:11:11 +0200

    fix typo

--- dev_only/TofGetScan.R
+++ dev_only/TofGetScan.R
@@ -1,8 +1,9 @@
 # get individual scan
-if (is.null(getGeneric("getScan")){
+if (is.null(getGeneric("getScan"))){
 setGeneric("getScan",
              function(object, index) standardGeneric("getScan"))
 }
+
 setMethod(f="getScan", 
           signature = signature("TofMeasurement","numeric"),
           definition=function(object, index=1){
