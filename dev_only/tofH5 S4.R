7223f2f1130e7d6538607406ab37ea26de9b5e5a
Clemens Ager <clemens.ager@uibk.ac.at>
2015-08-05 18:15:20 +0200

    open and close TOF files

--- dev_only/tofH5 S4.R
+++ dev_only/tofH5 S4.R
@@ -4,17 +4,8 @@ setClass("TofH5",
          representation(file.name='character',
          				n.scans='numeric', 
          				meas.reader='H5IdComponent',
+         				.tofbloc='H5IdComponent',
+         				.indexhelp='list',
          				.Data='list') 
          )
  
-# open tof file
-setGeneric("open", function(object) {
-  standardGeneric("open")
-})
-
-
-setMethod('open', signature(object = 'TofH5'), function(object) {
-			object@meas.reader <- H5Fopen(object@file.name)
-})
-
-
---
+++ dev_only/tofH5_file.R
@@ -0,0 +1,39 @@
+#' open TOF file
+#' 
+#' required for reading tof data
+#' @export
+#' @docType methods
+#' @rdname tofh5-methods
+setGeneric("open", function(object) {
+  standardGeneric("open")
+})
+
+#' @rdname tofh5-methods
+#' @aliases tofh5,open,open-method
+setMethod('open', signature(object = 'TofH5'), function(object) {
+			object@meas.reader <- H5Fopen(object@file.name)
+  object@.tofblock <- get.raw.tofblock(object@meas.reader)
+  object@indexhelp <- tof.indexhelp(object@.tofblock)
+})
+
+
+#' close TOF file
+#' 
+#' usually not required
+#' @export
+#' @docType methods
+#' @rdname tofh5-methods
+setGeneric("close", function(object) {
+  standardGeneric("close")
+})
+
+#' @rdname tofh5-methods
+#' @aliases tofh5,close,close-method
+setMethod('close', signature(object = 'TofH5'), function(object) {
+			H5Fclose(object@meas.reader)
+			object@meas.reader <- NULL
+  object@.tofblock <- NULL
+  object@indexhelp <- NULL
+})
+
+
