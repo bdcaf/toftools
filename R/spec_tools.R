18a6841f018b70fc798c605faa4b2eb009b0dafa
Clemens Ager <clemens.ager@gmail.com>
2017-02-22 14:41:21 +0100

    add minlen

--- R/spec_tools.R
+++ R/spec_tools.R
@@ -3,6 +3,7 @@
 #' @description For many functions it seems helpful to use a sparse
 #' spectrum instead of the dense one stored. A usual breath TOF
 #' spectrum of 400,000 indices has less than 1% values greater 0.
+#' Use lower and minlen to control granularity.
 #'
 #' The sparse format is a simple list where each entry has three
 #' fields: start the index of start point, rlen the length of signal
@@ -10,11 +11,12 @@
 #'
 #' @param full.wave a full dense TOF spectrum
 #' @param lower parameter setting the lower number of counts
+#' @param minlen minimum lenght of series > 0 - 
 #'
 #' @return list of sparse regions where the spectrum is > `lower`	
 #'
 #' @export
-sparse_spec <- function(full.wave, lower=0){
+sparse_spec <- function(full.wave, lower=0, minlen=5){
   idx <- full.wave > lower
   renc <- rle(as.vector(idx))
   startp <- cumsum( c(0, as.numeric(renc$lengths)))
@@ -27,7 +29,7 @@ sparse_spec <- function(full.wave, lower=0){
   dv <- data.frame(start=startp[seq_along(renc$lengths)], 
 				   rlen = renc$lengths, 
 				   greater0 = renc$values) %>%
-				filter(rlen >3, greater0) 
+				filter(greater0, rlen > minlen) 
 
   with(dv, mapply(extract_ts, start, rlen))
 }
