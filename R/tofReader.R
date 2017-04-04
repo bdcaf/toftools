f5c8f9f7ee2c18502b680fff30a8c7d0646d7549
Clemens Ager <clemens.ager@gmail.com>
2017-04-04 14:35:27 +0200

    mass axis helpr

--- R/tofReader.R
+++ R/tofReader.R
@@ -222,3 +222,45 @@ masscal_legacy_smooth <- function(fid, indexhelp){
 		   )
   select(ci, buf, write, a,b)
 }
+
+#' make a mass axis based on resolution
+#' no export
+#' @param resolution of the mass axis in 1/r
+#' @param mass.low starting point of mass axis (should be >0)
+#' @param mass.high upper limit of mass axis
+#' @return list of masses
+make_massaxis <- function( resolution=1e4,
+						   mass.low=10,
+						   mass.high=200){
+  log_mass_axis <- seq(from=log10(mass.low), 
+					   to = log10(mass.high),
+					   length.out=((mass.high-mass.low)*resolution)
+					   )
+  mass_axis <- 10^log_mass_axis
+}
+
+#' convert mass axis to indices in a specific spectrum
+#' no export
+#' @param calib containing a and b of mass calibration
+#' @param target mass axis
+#' @export vector of indices (non integer!)
+massaxis2ind <- function(calib, mass_axis){
+  with(calib,a*sqrt(mass_axis) + b)
+}
+
+
+#' extract resampled vector at mass axis
+#' incoming counts are expected to arrive uniformly in measurement
+#' interval
+#' no export
+#' @param spec vector of spectrum in raw format
+#' @param calib current mass calibration
+#' @mass_axis target mass axis
+#' @return vector of resampled counts - not it is open to the left
+#' with first value NA
+spec2mav <- function(spec, calib, mass_axis){
+  inds <- massaxis2ind(calib, mass_axis)
+  cspec <- cumsum(spec)
+  y2 <- approx(x=seq_along(cspec), y=cspec, xout=inds)$y
+  mav <- c(NA, diff(y2))
+}
