543080205e3e447f74cbdeeda80530258e0e3d38
Clemens Ager <clemens.ager@gmail.com>
2017-05-04 16:24:45 +0200

    sparse vec

--- R/spec_tools.R
+++ R/spec_tools.R
@@ -1,6 +1,42 @@
 library(dplyr)
 library(purrr)
 
+canMerge <- function(sp1, sp2, max_dif = 10) { (sp2[[1,'starts']] - sp1[[1,'ends']]) < max_dif }
+mergeSp <- function(sp1, sp2){ 
+  sp1$ends <- sp2$ends
+  sp1
+}
+
+mergeFun <- function(acc, cur, tib, max_dif){
+  if (canMerge(acc, cur, max_dif)){
+	tibout <- tib
+	acc2 <- mergeSp(acc, cur)
+  } else {
+	tibout <- rbind(tib, acc)
+	acc2 <- cur
+  }
+  list(acc = acc2, tib = tibout)
+}
+
+TibAccRed <- function(f, tibin, max_dif=10){
+  init <- data_frame()
+  acc <- tibin[1,]
+  tibin <- tibin[-1,]
+  
+  while (nrow(tibin) > 0){
+	cur <- tibin[1,]
+	tibin <- tibin[-1,]
+
+  	val <- f(acc, cur, init, max_dif)
+	acc <- val$acc
+	init <- val$tib
+  }
+
+  rbind(init, acc) 
+}
+
+#dd <- TibAccRed(mergeFun, only_sig)
+
 #' create a sparse representation of the full TOF scan
 #'
 #' @description For many functions it seems helpful to use a sparse
@@ -22,15 +58,24 @@ library(purrr)
 sparse_spec <- function(full.wave, lower=0, minlen=0){
   idx <- full.wave > lower
   renc <- rle(as.vector(idx))
-  startp <- cumsum( c(0, as.numeric(renc$lengths)))
 
-  extract_ts <- function(starter, len)
-	list(start = starter, 
-		 rlen = len,
-		 signal = full.wave[starter + seq_len(len)])
+  tmp <- 
+  	data_frame(len = renc$lengths, 
+  			   g0  = renc$values) %>%
+	mutate( ends = cumsum(len),
+		    starts = 1 + c(0, ends[-nrow(.)]) 
+		    )
 
-  calc_inds <- function(starter, len) starter + seq_len(length.out = len)
+  small0 <- tmp %>% filter(g0==F, len<3)
+
+  only_sig <- tmp %>% filter(g0) %>% select(starts, ends)
+  condensed_sig <- TibAccRed(mergeFun, only_sig, max_dif=20)
+
+  extract_ts <- function(starts, ends) list(full.wave[starts:ends])
+  condensed_sig %>% rowwise() %>% mutate(v=extract_ts(starts,ends))
+  # TODO check identity
 }
+
 
 inpterp_sparse <- function(spa, func){
 # TODO
@@ -38,3 +83,6 @@ inpterp_sparse <- function(spa, func){
 corr_sparse <- function(spa, spb){
 # TODO
 }
+
+
+
