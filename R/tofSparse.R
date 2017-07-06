9da7b04f2569c0454d229b744891c780cf09a1d3
Clemens Ager <clemens.ager@gmail.com>
2017-07-06 14:26:35 +0200

    documentation

--- R/tofSparse.R
+++ R/tofSparse.R
@@ -7,16 +7,18 @@ library(data.table)
 #' spectrum of 400,000 indices has less than 1% values greater 0.
 #' Use lower and minlen to control granularity.
 #'
-#' The sparse format is a simple list where each entry has three
+#' The sparse format is a simple data.table where each entry has three
 #' fields: start the index of start point, rlen the length of signal
 #' >0, and signal the actual values > 0.
 #'
 #' @param full.wave a full dense TOF spectrum
 #' @param lower parameter setting the lower number of counts
 #' @param minlen minimum lenght of series > 0 - 
+#' @param max_gap to further simplify densly store gaps below this
+#' number
 #'
 #' @return list of sparse regions where the spectrum is > `lower`	
-sparse_spec <- function(full.wave, lower=0, minlen=1, max_gap = 100){
+semisparse_spec <- function(full.wave, lower=0L, minlen=1L, max_gap = 30L){
   tab <- data.table(id=seq_along(full.wave), wave=full.wave)[wave>lower]
   tab[, gap:= (id - shift(id, fill=0))]
   tab[, new_block := gap>max_gap]
@@ -35,6 +37,14 @@ sparse_spec <- function(full.wave, lower=0, minlen=1, max_gap = 100){
   return( fullT[len>=minlen])
 }
 
+#' helper function to simplify sparse spectra 
+#'
+#' @description simplifies spectra by closing gaps smaller than max_gap.
+#' In the end it will not be usefull for correlation finding.  But it
+#' was the function that led to a number of improvements.
+#' @param spec_pre semisparse spectrum to simplify.
+#' @param max_gap maximum gap width allowed
+#' @return a potentially simplified spectrum
 simplify_sparse <- function(spec_pre, max_gap=10L){
   spec_pre[, gap := starts - shift(ends, fill = 0)][, inds := cumsum(gap > max_gap)]
   # close gaps but only within groups
@@ -52,6 +62,7 @@ simplify_sparse <- function(spec_pre, max_gap=10L){
   result
 }
 
+#' helper function for warping a single semisparse spectrum line
 warp_line <- function(warp_fun, starts, ends, v){
   i_trans <- warp_fun((starts-1):(ends+1))
   cuv <- cumsum(v)
@@ -64,9 +75,19 @@ warp_line <- function(warp_fun, starts, ends, v){
   list(starts=i_new[1],  ends=i_new[length(i_new)], v=as.vector(v_new))
 }
 
-
+#' helper to rearrange output of vectorize for data.table
 seq_list <- function(x,i) sapply(x, function(x) x[[i]])
+#' helper specifically sequencing warpline
 seq_wf <- function(tmp) lapply(1:3, function(a) seq_list(tmp,a))
+
+#' warps a complete semisparse spectrum by warpfun
+#'
+#' @description Does the lifting by recalculating a warped semisparse
+#' spectrum.
+#' @param semi_sparse semisparse spectrum
+#' @warp_fun a function that recalculates the indices.  Should be
+#' monotonic increasing
+#' @return warped semisparse spectrum
 warp_spec <- function(semi_sparse, warp_fun){
   vec_warp <- Vectorize( function(s,e,v) warp_line(warp_fun,s,e,v), SIMPLIFY=F)
   vec_order_warp <- function(s,e,v) seq_wf(vec_warp(s,e,v))
@@ -91,7 +112,18 @@ display.sparseTof = function(obj,...) {
   cat("A semi-sparse Tof data set.\n")
 }
 
-cor.semisparse.full <- function(aSpec, refSpec, refEnergy){
+#' calculates distance between spectra
+#'
+#' @description calculates cosine distance between a semisparse spectrum
+#' and a reference spectrum.  Practically the reference is a dense
+#' spectrum.
+#' @param aSpec semisparse spectrum
+#' @param refSpec reference spectrum
+#' @param refEnergy energy of the reference spectrum may be
+#' pre-calculated
+#' @return cosine distance of spectra
+#'
+cor.semisparse.full <- function(aSpec, refSpec, refEnergy=sum(refSpec^2)){
   aSpec[, energy := Vectorize(function(x) sum(x^2))(v)]
   aSpec[, sp := Vectorize( function(st,en,v) v %*% refSpec[st:en])(starts, ends,v)]
   agg <- aSpec[, .(total_energy=sum(energy), total_sp=sum(sp)), by=NULL]
@@ -99,18 +131,18 @@ cor.semisparse.full <- function(aSpec, refSpec, refEnergy){
 }
 
 
-#' revert sparse spectrum to dense
-#'
-#' @description Turns a sparse spectrum into a dense vector.  Mostly
-#' useful for quick testing.
-#' @param sparse.spec, sparse spectrum
-#' @param full.len, optional full length of dense spectrum
-#' @return vector
-#' @export
-sparse2dense <- function(sparse.spec, full.len=398999){
-  out <- vector(mode='numeric', length=full.len)
-  sparse.spec %>% rowwise() %>%
-  	do( x=with(., out[starts:ends] <<- c(unlist(v))))
+##' revert sparse spectrum to dense
+##'
+##' @description Turns a sparse spectrum into a dense vector.  Mostly
+##' useful for quick testing.
+##' @param sparse.spec, sparse spectrum
+##' @param full.len, optional full length of dense spectrum
+##' @return vector
+##' @export
+#sparse2dense <- function(sparse.spec, full.len=398999){
+  #out <- vector(mode='numeric', length=full.len)
+  #sparse.spec %>% rowwise() %>%
+      #do( x=with(., out[starts:ends] <<- c(unlist(v))))
   
-  out
-}
+  #out
+#}
