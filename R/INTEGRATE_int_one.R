23011d82c65a14945ea775ad78fbaa2c69ff913c
Clemens Ager <clemens.ager@gmail.com>
2018-05-01 10:18:14 +0200

    integration for file

--- R/INTEGRATE_int_one.R
+++ R/INTEGRATE_int_one.R
@@ -45,3 +45,18 @@ extract_tof <- function(tof_ob, peak_list) {
     ungroup() %>%
     mutate(numtime = as.double(time))
 }
+
+
+#' extract one measurement from file
+#' @param in_file hdf5 file
+#' @param request list as created by
+#' @export
+#' @import dplyr
+#'
+integrate_masses <-
+  function(in_file, request){
+    tof_ob <- new("TofClass", filename = in_file)
+    on.exit( finalize(tof_ob))
+
+    extract_tof(tof_ob, request)
+  }
