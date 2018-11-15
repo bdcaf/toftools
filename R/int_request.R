bfa5bba5f7e2a59dd57d6f996f93ebcfe3421e72
Clemens Ager <clemens.ager@gmail.com>
2018-11-15 10:40:33 +0100

    method for request for general mass cal function

--- R/int_request.R
+++ R/int_request.R
@@ -14,3 +14,18 @@ request_list <- function(masses, fitm)
         ceiling(),
       wid_ind = right_ind - left_ind
     )
+
+#' Function to create list of locations in tof file
+#' @import dplyr
+#' @export
+#' @param masses list of masses to integrate
+#' @param function to that can predict the location in original frame
+#' @return tibble with locations of peaks in tof frame
+request_mass <- function(masses, mass_fun)
+  tibble(name = names(masses), mass = unlist(masses)) %>%
+    mutate(
+      wid = sqrt(mass) / 300, low = mass - wid, high = mass + wid,
+      left_ind = mass_fun(low) %>% floor(),
+      right_ind = mass_fun(high) %>% ceiling(),
+      wid_ind = right_ind - left_ind
+    )
